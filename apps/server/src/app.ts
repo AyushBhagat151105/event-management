25fe3369b4dd32abad36b94728c24b893a596e98
Ayush_Bhagat <103896902+AyushBhagat151105@users.noreply.github.com>
2025-09-23 21:36:34 +0530

    feat(server): add cookie-parser and jsonwebtoken dependencies; implement JWT token generation

--- apps/server/src/app.ts
+++ apps/server/src/app.ts
@@ -1,13 +1,24 @@
 import express, { Express } from "express";
 import { errorHandler } from "./middlewares/errorHandler";
-const app: Express = express()
+import cors from "cors";
+import cookieParser from "cookie-parser";
+const app: Express = express();
 
-app.use(express.json())
+app.use(express.json());
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+app.use(cookieParser());
 app.get("/", (req, res) => {
-    res.status(200).json("It is up and running...")
-})
-app.use(express.urlencoded({ extended: true }))
+  res.status(200).json("It is up and running...");
+});
+app.use(express.urlencoded({ extended: true }));
 
-app.use(errorHandler)
+app.use(errorHandler);
 
-export default app
+export default app;
--- apps/server/src/validators/env.ts
+++ apps/server/src/validators/env.ts
@@ -1,18 +1,30 @@
-import { z } from "zod"
+import { z } from "zod";
 
 const envSchema = z.object({
-    PORT: z.string().optional(),
-    BASEURL: z.string().min(1, { message: "Base is required" }),
-    NODE_ENV: z.string().min(1, { message: "NODE_ENV is required" }).optional()
-})
+  PORT: z.string().optional(),
+  BASEURL: z.string().min(1, { message: "Base is required" }),
+  NODE_ENV: z.string().min(1, { message: "NODE_ENV is required" }).optional(),
+  ACCESSTOKEN: z
+    .string()
+    .min(1, { message: "Access token secret is required" }),
+  REFRESHTOKEN: z
+    .string()
+    .min(1, { message: "Refresh token secret is required" }),
+  ACCESSTOKEN_EXPIRE: z
+    .string()
+    .min(1, { message: "Access token expiry is required" }),
+  REFRESHTOKEN_EXPIRE: z
+    .string()
+    .min(1, { message: "Refresh token expiry is required" }),
+});
 function createENV(env: NodeJS.ProcessEnv) {
-    const validationResult = envSchema.safeParse(env)
+  const validationResult = envSchema.safeParse(env);
 
-    if (!validationResult.success) {
-        throw new Error(validationResult.error.message)
-    }
+  if (!validationResult.success) {
+    throw new Error(validationResult.error.message);
+  }
 
-    return validationResult.data
+  return validationResult.data;
 }
 
-export const env = createENV(process.env)
+export const env = createENV(process.env);
